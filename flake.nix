f7a2c8508b2ec6e09d93223749ebf979b51441a4
Antonio Gutierrez <antonio.gutierrez1@ibm.com>
2025-01-08 15:52:45 +0100

    flake.nix: update flake to latest
    Signed-off-by: Antonio Gutierrez <antonio.gutierrez1@ibm.com>

--- flake.nix
+++ flake.nix
@@ -73,9 +73,8 @@
       ];
 
       fonts.packages = with pkgs; [
-        nerdfonts
 	source-code-pro
-      ];
+      ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
      
       homebrew = {
         enable = true;
@@ -167,7 +166,7 @@
 	  rm -rf /Applications/Nix\ Apps
 	  mkdir -p /Applications/Nix\ Apps
 	  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-	  while read src; do
+	  while read -r src; do
 	    app_name=$(basename "$src")
 	    echo "copying $src" >&2
 	    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
